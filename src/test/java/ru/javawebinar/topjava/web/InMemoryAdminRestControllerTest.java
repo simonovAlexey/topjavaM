3966baaf44b22ea56d8988a6d487d54878323aaa
Simonov <3694369@gmail.com>
2016-12-26 16:55:31 +0300

    HW03 check SpringMain/InMemoryAdminRestControllerTest/ InMemoryAdminRestControllerSpringTest

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.DbPopulator;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
@@ -18,12 +19,14 @@ import static ru.javawebinar.topjava.UserTestData.USER;
 public class InMemoryAdminRestControllerTest {
     private static ConfigurableApplicationContext appCtx;
     private static AdminRestController controller;
+    private static DbPopulator dbPopulator;
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/spring-db.xml");
         System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
         controller = appCtx.getBean(AdminRestController.class);
+        dbPopulator = appCtx.getBean(DbPopulator.class);
     }
 
     @AfterClass
@@ -36,8 +39,9 @@ public class InMemoryAdminRestControllerTest {
         // Re-initialize
         UserRepository repository = appCtx.getBean(UserRepository.class);
         repository.getAll().forEach(u -> repository.delete(u.getId()));
-        repository.save(USER);
-        repository.save(ADMIN);
+//        repository.save(USER);
+//        repository.save(ADMIN);
+        dbPopulator.execute();
     }
 
     @Test
