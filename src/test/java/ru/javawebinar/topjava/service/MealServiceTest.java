22db681c66824df7fadd43eca4273c60458c0dd6
Simonov <3694369@gmail.com>
2017-01-18 14:02:32 +0300

    hw05-2

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -34,7 +34,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(Profiles.ACTIVE_DB)
+@ActiveProfiles({Profiles.ACTIVE_DB,Profiles.DATAJPA})
 public class MealServiceTest {
     private static final Logger LOG = LoggerFactory.getLogger(MealServiceTest.class);
     private static StringBuilder results = new StringBuilder();
--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -27,7 +27,7 @@ import static ru.javawebinar.topjava.UserTestData.*;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles({Profiles.ACTIVE_DB,Profiles.JDBC})
+@ActiveProfiles({Profiles.ACTIVE_DB,Profiles.DATAJPA})
 public class UserServiceTest {
 
     @Autowired
